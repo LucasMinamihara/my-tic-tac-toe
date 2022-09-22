ca284561594a090acc45eef922cd10e2516ab614
Lucas Minamihara <Lucas.hmds@outlook.com>
2022-09-22 18:16:15 -0300

    Initial commit 2

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ let eachTurn = 0;
 const allSquares = document.querySelectorAll(".square");
 allSquares.forEach((eachSquare) => {
   eachSquare.addEventListener("click", function () {
-    if (eachTurn) {
-      this.style.backgroundColor = "red";
-      eachTurn = 0;
-    } else {
-      this.style.backgroundColor = "blue";
-      eachTurn = 1;
+    if (!this.style.backgroundColor) {
+      if (eachTurn) {
+        this.style.backgroundColor = "red";
+        eachTurn = 0;
+      } else {
+        this.style.backgroundColor = "blue";
+        eachTurn = 1;
+      }
     }
   });
 });
